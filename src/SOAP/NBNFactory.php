9dde3569bd794ada17373749eeea1536f1abcc88
kadhamw <kade@procomputers.com.au>
2021-07-22 16:09:54 +1000

    Ensure a array is always returned

--- src/SOAP/NBNFactory.php
+++ src/SOAP/NBNFactory.php
@@ -25,6 +25,7 @@ class NBNFactory
 
         $params = $result->Parameters->Param;
         $converted = $conn->ConvertPARAMStoOBJ($params); // Change to WsdlResponse
+        if (!is_array($converted)) {$converted = [$converted];}
         return($converted);
     }
 
